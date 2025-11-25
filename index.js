fd567124d01480c4c2715d14fd768ad68301d339
jalila120 <aljalila2024@gmail.com>
2025-11-25 18:53:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors("*"));
 app.use(require("morgan")("dev"));
 
 const emailData = {
-  user: "akeuiop@gmail.com",
-  pass: "cgyf cukh ahdw nbdy",
+  user: "absheruiop@gmail.com",
+  pass: "sonr nnkx uglr kzqv",
   // user: "saudiabsher1990@gmail.com",
   // pass: "qlkg nfnn xaeq fitz",
 };
@@ -342,6 +342,7 @@ mongoose
       console.log("server running and connected to db" + conn.connection.host);
     })
   );
+
 
 
 
